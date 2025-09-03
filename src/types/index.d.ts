0aa7e9a2024d962eb799984977860121d53e349d
Meiyi <ant19950601@gmail.com>
2025-09-03 16:41:47 +0800

    :label: Add Work setting typesciprt

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -3,3 +3,11 @@ type SkillsItemType = {
   icon:React.ReactNode,
   skills:string[]
 }
+
+type WorksItemType = {
+  job_title?:string,
+  title:string,
+  start_year:string,
+  end_year:string,
+  description:string[] | { project:string, text:string[] }[]
+}
