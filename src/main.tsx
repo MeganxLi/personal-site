4ad745171ff6827f0f044eb0036a55ff21da7c6b
Meiyi <ant19950601@gmail.com>
2025-09-05 04:09:55 +0800

    :bug: fix: i18next [n.forEach is not a function] bug

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
 import { StrictMode } from 'react'
 
 import { createRoot } from 'react-dom/client'
+import { I18nextProvider } from 'react-i18next'
 import { RouterProvider } from 'react-router-dom'
 
 import './styles/App.scss'
 
+import i18n from './i18n'
 import router from './router/router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
-
+    <I18nextProvider i18n={i18n}>
+      <RouterProvider router={router} />
+    </I18nextProvider>
   </StrictMode>,
 )
