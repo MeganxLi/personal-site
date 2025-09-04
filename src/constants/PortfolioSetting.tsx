6df8063af8e95391e430d536ec7f00d3d058306c
Meiyi <ant19950601@gmail.com>
2025-09-04 15:23:50 +0800

    :sparkles: Update Portofoli page grid

--- src/constants/PortfolioSetting.tsx
+++ src/constants/PortfolioSetting.tsx
@@ -1,6 +1,29 @@
 export const github_url = 'https://github.com/MeganxLi/'
 export const github_page = 'https://meganxli.github.io/'
 
+export const AnimationEnum = {
+  expand: 'expand',
+  shrink: 'shrink',
+  idle: 'idle',
+} as const
+
+export const TagEnum = {
+  react: 'React',
+  type: 'Typescript',
+  vite: 'Vite',
+  linaria: 'Linaria',
+  styled: 'Styled-Components',
+  fabric: 'Fabric.js',
+  pdf: 'React-pdf',
+  tailwind: 'TailwindCSS',
+  jotai: 'Jotai',
+  ant: 'Ant Design',
+  beautiful: 'React-beautiful-dnd',
+  gsap: 'GSAP',
+  redux: 'React-Redux',
+  vue: 'Vue',
+}
+
 const PortfolioSetting : PortfolioItemType[] = [
   {
     title: '簡易易經占卜',
@@ -8,6 +31,7 @@ const PortfolioSetting : PortfolioItemType[] = [
     img: true,
     github: true,
     figma: 'https://www.figma.com/design/KX81j9dbU03YY9kCdAmNeF/%E7%B0%A1%E6%98%93%E6%98%93%E7%B6%93?node-id=0-1&m=dev&t=PG7X6StCf5Vzs4w4-1',
+    tag: [TagEnum.react, TagEnum.type, TagEnum.vite],
   },
   {
     title: '咖啡清單',
@@ -15,12 +39,14 @@ const PortfolioSetting : PortfolioItemType[] = [
     img: true,
     github: true,
     figma: 'https://www.figma.com/design/KF0SZWlRq5npZeA7EJVgtY/%E5%92%96%E5%95%A1%E6%B8%85%E5%96%AE-coffee-guide?node-id=2-224&m=dev&t=bJfvRdEaEIDkXGOj-1',
+    tag: [TagEnum.react, TagEnum.linaria, TagEnum.type, TagEnum.vite],
   },
   {
     title: '角色投影介紹',
     link: 'character-spotlight',
     img: true,
     github: true,
+    tag: [TagEnum.react, TagEnum.styled, TagEnum.type, TagEnum.vite],
   },
   {
     title: '時間計算機',
@@ -28,30 +54,36 @@ const PortfolioSetting : PortfolioItemType[] = [
     img: true,
     github: true,
     figma: 'https://www.figma.com/design/rTNa1ZqefedGHjTqQY6ghc/%E6%99%82%E9%96%93%E8%A8%88%E6%99%82%E5%99%A8-Time-Computer?node-id=2-229&m=dev&t=fDPQwlcUWuHNB4v4-1',
+    tag: [TagEnum.react, TagEnum.ant, TagEnum.vite],
   },
   {
     title: '點點簽',
     link: 'dotted-sign',
     img: true,
     github: true,
+    tag: [TagEnum.react, TagEnum.tailwind, TagEnum.jotai, TagEnum.pdf, TagEnum.fabric],
   },
   {
     title: 'Scrum 互動頁面',
     link: 'scrum-xinshou-village',
     img: true,
     github: true,
+    tag: [TagEnum.react, TagEnum.type, TagEnum.beautiful],
+
   },
   {
     title: 'THE F2E 互動式網頁',
     link: 'parallax-scrolling-website',
     img: true,
     github: true,
+    tag: [TagEnum.react, TagEnum.linaria, TagEnum.gsap],
   },
   {
     title: '仿台灣高鐵站的電子列車時刻表',
     link: 'thsr-departure-board',
     img: true,
     github: true,
+    tag: [TagEnum.react, TagEnum.styled, TagEnum.redux],
   },
   {
     title: '台灣銀行清單',
@@ -59,6 +91,7 @@ const PortfolioSetting : PortfolioItemType[] = [
     img: true,
     github: true,
     figma: 'https://www.figma.com/design/fKzy5AUKIlKqAqkCLWddld/%E5%8F%B0%E7%81%A3%E9%8A%80%E8%A1%8C%E6%B8%85%E5%96%AE?node-id=0-1&m=dev&t=UvuG6O5vvgMg3SuC-1',
+    tag: [TagEnum.vue, TagEnum.tailwind],
   },
   {
     title: 'React Calculator 計算機',
@@ -66,6 +99,7 @@ const PortfolioSetting : PortfolioItemType[] = [
     img: true,
     github: true,
     figma: 'https://www.figma.com/design/nLI1f51bNMDNBzm5GC5n6I/%E8%A8%88%E7%AE%97%E6%A9%9F-Calculator?node-id=0-1&m=dev&t=JGZnjult1GsmbKxN-1',
+    tag: [TagEnum.vue, TagEnum.styled],
   },
 
 ]
--- src/page/Portfolio.tsx
+++ src/page/Portfolio.tsx
@@ -1,29 +1,155 @@
-import { Figma, Github } from 'lucide-react'
+import { useEffect, useState } from 'react'
 
-import PortfolioSetting, { github_page, github_url } from '../constants/PortfolioSetting'
+import {
+  ArrowLeft, ArrowRight, ChevronRight, Figma, Github,
+} from 'lucide-react'
+import { motion } from 'motion/react'
 
-const Portfolio = () => (
-  <div id="Portfolio" className="appear">
-    <h2 className="portfolio-title">Portfolio</h2>
-    <div className="portfolio-button">
-      <button type="button">Web</button>
-      <button type="button">UIUX</button>
-    </div>
-    <div className="portfolio-list">
-      {PortfolioSetting.map((item, k) => (
-        <a className="portfolio-item" key={k} href={github_page + item.link} target="_blank" rel="noreferrer">
-          <div className="portfolio-img" style={{ backgroundImage: `url('${import.meta.env.BASE_URL}/${item.link}.png')` }} />
-          <p className="portfolio-item-title">
-            {`# ${item.title}`}
-          </p>
-          <div className="portfolio-item-link">
-            {item.github && <a href={github_url + item.link} target="_blank" rel="noreferrer"><Github strokeWidth={1.5} /></a>}
-            {item.figma && <a href={item.figma} target="_blank" rel="noreferrer"><Figma strokeWidth={1.5} /></a>}
+import PortfolioSetting, { AnimationEnum, github_page, github_url } from '../constants/PortfolioSetting'
+
+const Portfolio = () => {
+  const [tempIndex, setTempIndex] = useState(0)
+  const [carouselIndex, setCarouselIndex] = useState(0)
+  const [phase, setPhase] = useState<PortfolioAnimationType>(AnimationEnum.idle)
+  const [disabledButton, setDisabledButton] = useState(false)
+
+  const clickStartAnimation = () => {
+    setPhase(AnimationEnum.expand)
+    setDisabledButton(true)
+  }
+
+  const prevIndex = () => {
+    clickStartAnimation()
+
+    if (carouselIndex > 0){
+      setTempIndex(carouselIndex - 1)
+    } else {
+      setTempIndex(PortfolioSetting.length - 1)
+    }
+  }
+
+  const nextIndex = () => {
+    clickStartAnimation()
+
+    if (carouselIndex < PortfolioSetting.length - 1){
+      setTempIndex(carouselIndex + 1)
+    } else {
+      setTempIndex(0)
+    }
+  }
+
+  const jumpIndex = (k:number) => {
+    clickStartAnimation()
+    setTempIndex(k)
+  }
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      nextIndex()
+    }, 8000)
+
+    return () => {
+      clearInterval(timer)
+    }
+  }, [carouselIndex])
+
+  return (
+    <div id="Portfolio" className="appear">
+      <div className="portfolio-top">
+        <h3 className="portfolio-title">Portfolio</h3>
+        <div className="portfolio-carousel-button">
+          <button type="button" onClick={prevIndex} disabled={disabledButton}><ArrowLeft size={16} /></button>
+          <button type="button" onClick={nextIndex} disabled={disabledButton}><ArrowRight size={16} /></button>
+        </div>
+        <button type="button" className="portfolio-all">
+          View All
+          <ChevronRight size={14} />
+        </button>
+      </div>
+      <ul className="portfolio-carousel-content">
+        {PortfolioSetting.map((item, k) => (
+          <li
+            className={`portfolio-carousel-item ${carouselIndex === k ? 'checked' : ''} ${disabledButton ? 'disable' : ''}`}
+            key={item.link}
+            onClick={() => jumpIndex(k)}
+          />
+        ))}
+      </ul>
+      <div className="portfolio-list">
+        <div
+          className="portfolio-item white"
+          style={{ zIndex: phase !== AnimationEnum.idle ? 1000 : 0 }}
+        >
+          <div className="portfolio-item-content">
+            <motion.div
+              key={tempIndex}
+              className="white-cover"
+              initial={{ width: 0 }}
+              animate={{ width: '100%' }}
+              exit={{ width: 0 }}
+              transition={{ duration: 1, ease: 'easeInOut' }}
+              onAnimationComplete={() => {
+                setCarouselIndex(tempIndex)
+              }}
+            />
           </div>
-        </a>
-      ))}
+          <div className="portfolio-item-img">
+            {phase === AnimationEnum.expand && (
+            <motion.div
+              key="expand"
+              className="white-cover"
+              style={{ transformOrigin: 'left' }}
+              initial={{ scaleX: 0 }}
+              animate={{ scaleX: 1 }}
+              exit={{ scaleX: 0 }}
+              transition={{ duration: 0.4, ease: [0.4, 0, 0.2, 1] }}
+              onAnimationComplete={() => {
+                setPhase(AnimationEnum.shrink)
+                setCarouselIndex(tempIndex)
+                setDisabledButton(false)
+              }}
+            />
+            )}
+
+            {phase === AnimationEnum.shrink && (
+            <motion.div
+              style={{ transformOrigin: 'right' }}
+              key="shrink"
+              className="white-cover"
+              initial={{ scaleX: 1 }}
+              animate={{ scaleX: 0 }}
+              exit={{ scaleX: 1 }}
+              transition={{ duration: 0.4, ease: [0.4, 0, 0.2, 1] }}
+              onAnimationComplete={() => setPhase(AnimationEnum.idle)}
+            />
+            )}
+          </div>
+        </div>
+        {PortfolioSetting.map((item, k) => (
+          <a
+            key={k}
+            className="portfolio-item"
+            href={github_page + item.link}
+            target="_blank"
+            style={carouselIndex === k ? { zIndex: 1, opacity: 1 } : { zIndex: 0, opacity: 0 }}
+            rel="noreferrer"
+          >
+            <div className="portfolio-item-content">
+              <h5 className="portfolio-item-title">{item.title}</h5>
+              {item.tag?.map((tag) => <p key={tag}>{`# ${tag}`}</p>)}
+              <div className="portfolio-item-link">
+                <a href={github_url + item.link} target="_blank" rel="noreferrer"><Github strokeWidth={1.5} /></a>
+                {item.figma && <a href={item.figma} target="_blank" rel="noreferrer"><Figma strokeWidth={1.5} /></a>}
+              </div>
+            </div>
+            <div className="portfolio-item-img">
+              <img src={`${import.meta.env.BASE_URL}/${item.link}.png`} alt="" />
+            </div>
+          </a>
+        ))}
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Portfolio
