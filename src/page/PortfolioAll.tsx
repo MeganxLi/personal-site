e3e4549fedcba87d718112e3b61967bd95f2f660
Meiyi <ant19950601@gmail.com>
2025-09-05 19:12:19 +0800

    :memo: Add Protfolio All page

--- src/page/PortfolioAll.tsx
+++ src/page/PortfolioAll.tsx
@@ -1,29 +1,81 @@
-import { Figma, Github } from 'lucide-react'
+import { useState } from 'react'
 
+import i18next from 'i18next'
+import { Figma, Github, SlidersHorizontal } from 'lucide-react'
+import { AnimatePresence, motion } from 'motion/react'
+
+import Footer from '../components/Footer'
 import PortfolioSetting, { github_page, github_url } from '../constants/PortfolioSetting'
+import { smartLink } from '../until/smartLink'
 
-const PortfolioAll = () => (
-  <div id="Portfolio" className="appear">
-    <h2 className="portfolio-title">Portfolio</h2>
-    <div className="portfolio-button">
-      <button type="button">Web</button>
-      <button type="button">UIUX</button>
-    </div>
-    <div className="portfolio-list">
-      {PortfolioSetting.map((item, k) => (
-        <a className="portfolio-item" key={k} href={github_page + item.link} target="_blank" rel="noreferrer">
-          <div className="portfolio-img" style={{ backgroundImage: `url('${import.meta.env.BASE_URL}/${item.link}.png')` }} />
-          <p className="portfolio-item-title">
-            {`# ${item.title}`}
-          </p>
-          <div className="portfolio-item-link">
-            {item.github && <a href={github_url + item.link} target="_blank" rel="noreferrer"><Github strokeWidth={1.5} /></a>}
-            {item.figma && <a href={item.figma} target="_blank" rel="noreferrer"><Figma strokeWidth={1.5} /></a>}
+const PortfolioAll = () => {
+  const { t } = i18next
+
+  const [portfolioList, setPortfolioList] = useState(PortfolioSetting)
+
+  const filterPortfolio = (ui:boolean) => {
+    if (ui){
+      setPortfolioList(PortfolioSetting.filter((item) => item.figma))
+    } else {
+      setPortfolioList(PortfolioSetting)
+    }
+  }
+
+  return (
+    <>
+      <div id="PortfolioAll" className="appear">
+        <div className="portfolio-up">
+          <div className="portfolio-title">
+            <h2>{t('Portfolio.title')}</h2>
+            <a href="/" className="portfolio-title-home">Home</a>
           </div>
-        </a>
-      ))}
-    </div>
-  </div>
-)
+          <div className="portfolio-button">
+            <SlidersHorizontal size={16} />
+            <button type="button" className="button-text" onClick={() => filterPortfolio(false)}> All Web </button>
+            <button type="button" className="button-text" onClick={() => filterPortfolio(true)}>UIUX</button>
+          </div>
+        </div>
+        <div className="portfolio-list">
+          <AnimatePresence mode="wait">
+            {portfolioList.map((item) => (
+              <motion.a
+                className="portfolio-item"
+                key={item.link}
+                href={github_page + item.link}
+                target="_blank"
+                rel="noreferrer"
+                initial={{ opacity: 0, scale: 0 }}
+                animate={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0, scale: 0 }}
+              >
+                <div className="portfolio-img" style={{ backgroundImage: `url('${import.meta.env.BASE_URL}/${item.link}.png')` }} />
+                <p className="portfolio-item-title">
+                  {`# ${t(item.title)}`}
+                </p>
+                <div className="portfolio-item-link">
+                  {item.github && (
+                  <span
+                    onClick={() => smartLink(github_url + item.link)}
+                  >
+                    <Github strokeWidth={1.5} />
+                  </span>
+                  )}
+                  {item.figma && (
+                  <span
+                    onClick={() => smartLink(item.figma!)}
+                  >
+                    <Figma strokeWidth={1.5} />
+                  </span>
+                  )}
+                </div>
+              </motion.a>
+            ))}
+          </AnimatePresence>
+        </div>
+      </div>
+      <Footer homeLink="/" />
+    </>
+  )
+}
 
 export default PortfolioAll
